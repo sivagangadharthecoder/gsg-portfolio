e1198e6fbbcfd2fd69016e382da3ff2bab1c7749
Siva Gangadhar G. <119839967+sivagangadharthecoder@users.noreply.github.com>
2025-02-22 19:05:27 +0530

    Update Hero.jsx

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import './Hero.css';
 import siva from '../../assets/siva.jpeg';
-import resume from '../../assets/1298CVFINALS.pdf'; 
+import resume from '../../assets/1298CVFINALSS.pdf'; 
 
 const Hero = () => {
     return (
